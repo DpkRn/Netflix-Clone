298eec5bf4d22ec3cf9b0edb9b0429799f37a7e8
DpkRn <dwizardtechno@gmail.com>
2024-10-06 22:36:08 +0530

    mongo url fixed

--- netflix-server/index.js
+++ netflix-server/index.js
@@ -1,36 +1,43 @@
 // const express=require('express')
-import express from 'express'
-import dotenv from 'dotenv'
-import dbConnection from './utils/database.js'
-import cookieParser from 'cookie-parser'
-import userRoute from './routes/userRoute.js'
-import cors from 'cors'
-dbConnection();
+import express from "express";
+import dotenv from "dotenv";
+import dbConnection from "./utils/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import cors from "cors";
+
 dotenv.config({
-    path:".env"
-})
-const app=express();
+  path: ".env",
+});
+const app = express();
 
 //middlewares
-app.use(cors({
-    origin:[process.env.ORIGIN],
-    secure:true,
-    credentials:true
-}))
-app.use(express.urlencoded({extended:true}));
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    secure: true,
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.get('/',(req,res)=>{
-    return res.send("welcome to netfilx page !")
-})
+app.get("/", (req, res) => {
+  return res.send("welcome to netfilx page !");
+});
 
 //apis
-app.use('/api/v1/user',userRoute)
+app.use("/api/v1/user", userRoute);
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connection done !");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-
-
-app.listen(process.env.PORT,()=>{
-    console.log("listening to port http://localhost:8080")
-})
+app.listen(process.env.PORT, () => {
+  console.log("listening to port http://localhost:8080");
+});
--- netflix-server/utils/database.js
+++ netflix-server/utils/database.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose"
 import dotenv from 'dotenv'
 dotenv.config()
 const databaseConnection=()=>{
-   mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log('connection done !')
-   }).catch((err)=>{
-console.log(err)
-   })
+   
 }
 export default databaseConnection;
